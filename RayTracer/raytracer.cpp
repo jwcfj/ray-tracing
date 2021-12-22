205b2125a04b5891c924416be1d29d73d77478b2
mcdag <mcdag@cin.ufpe,br>
2021-12-22 19:52:59 -0300

    Função mix de efeitos, adição da variável recursiva e correção de comentários

--- RayTracer/raytracer.cpp
+++ RayTracer/raytracer.cpp
@@ -8,41 +8,41 @@
 
 #define EPSILON 0.0000001
 
-/* generics */
+// generics
 template<typename T>
 struct Vec3 {
   T x, y, z;
 
-  /* contructors */
-  /* se não for passado nada para o construtor, inicializa tudo como 0 */
+  // contructors
+  // se não for passado nada para o construtor, inicializa tudo como 0
   Vec3() {
     this->x = T(0);
     this->y = T(0);
     this->z = T(0);
   };
 
-  /* se for passado somente um número w ao construtor, inicializa tudo como w */
+  // se for passado somente um número w ao construtor, inicializa tudo como w
   Vec3(T w) {
     this->x = w;
     this->y = w;
     this->z = w;
   };
 
-  /* se forem dados os três valores, incializa como os valores */
+  // se forem dados os três valores, incializa como os valores
   Vec3(T x, T y, T z) {
     this->x = x;
     this->y = y;
     this->z = z;
   };
 
-  /* methods */
-  /* calcula o comprimento do vetor */
+  // methods
+  // calcula o comprimento do vetor
   T length() const { 
     const T length2 = x * x + y * y + z * z; 
     return sqrt(length2);
   };
   
-  /* normaliza o vetor -> comprimento = 1 */
+  // normaliza o vetor -> comprimento = 1
   Vec3& normalize(){
     const T length = length();
 
@@ -55,7 +55,7 @@ struct Vec3 {
     return *this;
   };
 
-  /* vetor + vetor */
+  // vetor + vetor
   Vec3<T> operator + (const Vec3<T> &v) const {
     return Vec3<T>(x + v.x, y + v.y, z + v.z);
   };
@@ -68,17 +68,17 @@ struct Vec3 {
     return *this;
   };
 
-  /* vetor - vetor */
+  // vetor - vetor
   Vec3<T> operator - (const Vec3<T> &v) const {
     return Vec3<T>(x - v.x, y - v.y, z - v.z);
   }
 
-  /* vetor * número */
+  // vetor * número
   Vec3<T> operator * (const T &f) const {
     return Vec3<T>(x * f, y * f, z * f);
   };
   
-  /* vetor * vetor */
+  // vetor * vetor
   Vec3<T> operator * (const Vec3<T> &v) const {
     return Vec3<T>(x * v.x, y * v.y, z * v.z);
   };
@@ -96,12 +96,12 @@ struct Vec3 {
     return Vec3<T>(-x, -y, -z);
   };
 
-  /* produto escalar */
+  // produto escalar
   T dot(const Vec3<T> &v) const {
     return x * v.x + y * v.y + z * v.z;
   };
 
-  /* produto vetorial */
+  // produto vetorial
   Vec3<T> cross(const Vec3<T> &v) const {
     Vec3<T> r;
     r.x = y * v.z - z * v.y;
@@ -111,7 +111,7 @@ struct Vec3 {
     return r;
   };
 
-  /* retorna o vetor printado */
+  // retorna o vetor printado
   friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v) { 
     os << "[" << v.x << " " << v.y << " " << v.z << "]"; 
     return os; 
@@ -127,7 +127,7 @@ struct Sphere {
   Vec3f surfaceColor, emissionColor;
   float transparency, reflection;
 
-  /* constructor */
+  // constructor
   Sphere(const Vec3f &c, const float &r, const Vec3f &sc, const float &refl = 0, const float &transp = 0, const Vec3f &ec = 0) {
     this->center = c;
     this->radius = r;
@@ -139,28 +139,28 @@ struct Sphere {
   };
 
   bool intersect(const Vec3f &rayOrig, const Vec3f &rayDir, float &t0, float &t1) const {
-    /* l vai ser o vetor que aponta da origem do raio de luz para o centro */
+    // l vai ser o vetor que aponta da origem do raio de luz para o centro
     Vec3f l = center - rayOrig;
-    /* tca vai ser o produto escalar de l com a direção do raio */
+    // tca vai ser o produto escalar de l com a direção do raio
     float tca = l.dot(rayDir);
 
-    /* se tca for igual a 0, quer dizer que l é perpendicular à direção do raio, logo não intersecta */
-    /* se tca for menor a 0, quer dizer que l é obtuso à direção do raio, logo, tem componentes x e y, 
-    porém em sentindo contrário à esfera, logo, não intersecta */
+    // se tca for igual a 0, quer dizer que l é perpendicular à direção do raio, logo não intersecta
+    // se tca for menor a 0, quer dizer que l é obtuso à direção do raio, logo, tem componentes x e y, 
+    //porém em sentindo contrário à esfera, logo, não intersecta
     if (tca < 0) {
       return false;
     }
 
-    /* fórmula para descobrir se um ponto pertence a uma esfera ->||p(t) - c||² = r² */
-    /* l.dot(l) calcula ||l||² e tca * tca calcula o comprimento */
+    // fórmula para descobrir se um ponto pertence a uma esfera ->||p(t) - c||² = r²
+    // l.dot(l) calcula ||l||² e tca * tca calcula o comprimento
     float d2 = l.dot(l) - tca * tca; 
     
-    /* se d > r, o raio n intersecta a esfera */
+    // se d > r, o raio n intersecta a esfera
     if (d2 > radius2) {
       return false;
     }
 
-    /* caso contratrio, intersecta, podendo ter 1 ou 2 pontos de intersecção */
+    // caso contratrio, intersecta, podendo ter 1 ou 2 pontos de intersecção
     float thc = sqrt(radius2 - d2); 
     t0 = tca - thc; 
     t1 = tca + thc; 
@@ -175,7 +175,7 @@ struct Triangle {
   Vec3f surfaceColor, emissionColor;
   float transparency, reflection;
 
-  /* constructor */
+  // constructor
   Triangle(const Vec3f &v1, const Vec3f &v2, const Vec3f &v3, const Vec3f &sc, const float &refl = 0, const float &transp = 0, const Vec3f &ec = 0) {
     this->vertex1 = v1;
     this->vertex2 = v2;
@@ -186,7 +186,7 @@ struct Triangle {
     this->reflection = refl;
   };
 
-  /*  Möller–Trumbore ray-triangle intersection algorithm */
+  //  Möller–Trumbore ray-triangle intersection algorithm
   bool intersect(Vec3f rayOrig, Vec3f rayDir, Triangle* inTriangle, Vec3f& outIntersectionPoint) {
     Vec3f vertex1 = inTriangle->vertex1;
     Vec3f vertex2 = inTriangle->vertex2;  
@@ -240,7 +240,7 @@ struct Plane {
   Vec3f surfaceColor, emissionColor;
   float transparency, reflection;
 
-  /* constructor */
+  // constructor
   Plane(const Vec3f &d, const Vec3f &n, const Vec3f &sc, const float &refl = 0, const float &transp = 0, const Vec3f &ec = 0) {
     this->dot = d;
     this->normal = n;
@@ -265,3 +265,10 @@ struct Plane {
   };
 };
 
+// This variable controls the maximum recursion depth
+#define MAX_RAY_DEPTH 5 
+
+// Função para fazer o mix entre relfexão e refração
+float mix(const float &a, const float &b, const float &mix) { 
+  return b * mix + a * (1 - mix); 
+};
